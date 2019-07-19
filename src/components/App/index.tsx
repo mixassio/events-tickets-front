f4c2919997db1b6e1afda4e3f60fb2e932c8d1da
mixassio <mixail.bondarev@gmail.com>
2019-07-19 20:10:52 +0300

    login page redy to start

---
+++ src/components/App/index.tsx
@@ -0,0 +1,15 @@
+import React  from 'react';
+// import Dashboard from '../Dashboard';
+import Auth from '../Auth';
+
+const App = (props: any) => {
+    return <Auth />
+    /*
+    if (!isAuth) return <Auth />;
+        return (
+            <Dashboard />
+        );
+    */
+};
+
+export default App;
---
+++ src/components/Auth/index.tsx
@@ -0,0 +1,10 @@
+import React  from 'react';
+
+const Auth = (props: any) => {
+    return (
+      <div>Hello, world</div>
+    )
+};
+
+
+export default Auth;
