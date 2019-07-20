b763a95c1f0531e1351f3080b2a29e8d2878e630
Pasechnik Vladislav <develop.pvv@gmail.com>
2019-07-20 12:55:17 +0300

    Describe project structure example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
+import React from "react";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      Hello, World!
-    </div>
-  );
-};
+const App = () => <div className="App">Hello, World!</div>;
 
 export default App;
